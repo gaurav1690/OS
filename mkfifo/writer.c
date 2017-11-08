037622324c4a43077c1a759f02376accdfbcea86
Gourav Singh <33493486+gaurav1690@users.noreply.github.com>
2017-11-08 23:11:02 +0530

    mkfifo

---
+++ mkfifo/writer.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+int main()
+{
+	int fd,clos;
+	
+	ssize_t retsize;
+	fd=open("namedpipe",O_WRONLY);//int open(const char *pathname, int flags, mode_t mode);
+	if(fd==-1)
+	{
+		perror("fd");
+	}
+	else
+	{
+		retsize=write(fd,"Hello\n",6);//ssize_t write(int fd, const void *buf, size_t count);
+		if(retsize==0)
+		{
+			printf("Data not written\n");	
+		}
+		else if(retsize==-1)
+		{
+			perror("retsize");
+		}
+		else
+		{
+			 clos=close(fd);//int close(int fd);
+			 if(clos==0)
+			 {
+			 	perror("clos");
+			 	return 0;
+			 }
+			 else if(clos==-1)
+			 {
+			 	perror("clos");
+			 	return 0;
+			 }
+		}
+		
+	}
+	
+	return 0;
+}
