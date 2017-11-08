5dbbd0a0793fe3c0317faeb302e2ea23417118bd
Gourav Singh <33493486+gaurav1690@users.noreply.github.com>
2017-11-08 23:13:32 +0530

    system call files

---
+++ syscallfile/fileread.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+
+int main()
+{
+	int fd,clos;
+	
+	unsigned char buffer[128];
+	int retsize;//ssize_t retsize;
+	
+	fd=open("demowrite.txt", O_RDONLY, S_IRUSR);//int open(const char *pathname, int flags, mode_t mode);
+	if(fd==-1)
+	{
+		perror("fd");
+	}
+	else
+	{
+		//ssize_t read(int fd, void *buf, size_t count);
+		retsize=read(fd,buffer,128);
+		if(retsize==0)
+		{
+			printf("nothing to read\n");	
+		}
+		else if(retsize==-1)
+		{
+			perror("retsize");
+		}
+		else
+		{
+			 printf("read:%s",buffer);
+			 clos=close(fd);//int close(int fd);
+			 if(clos==0)
+			 {
+			 	perror("clos");
+			 	return 0;
+			 }
+			 else if(clos==-1)
+			 {
+			 	perror("clos");
+			 	return 0;
+			 }
+		}
+		
+	}
+	
+	return 0;
+}
+
